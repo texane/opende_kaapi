3525dab8b47bfee668cb64529ce5103ca085b892
le mentec fabien <fabien.lementec@imag.fr>
2011-07-18 03:25:20 -0500

    [fix] do_configure use opende_kaapi directory

--- xkaapi/scripts/do_configure.sh
+++ xkaapi/scripts/do_configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ODE_ROOT=$HOME/repo/opende.git
+ODE_ROOT=$HOME/repo/opende_kaapi
 
 LD_LIBRARY_PATH="$HOME/install/xkaapi_rose/lib" \
 LDFLAGS="-L$HOME/install/xkaapi_rose/lib" \
